c4ec666584d4f669ca00781629243e5bbe4ca61b
sergiotorres262 <sergio2002ojedatorres@gmail.com>
2023-05-05 19:01:58 +0200

    autotimeout v1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, EmbedBuilder, ButtonBuilder, ActionRowBuilder, SelectMenuBuilder, Collection } = require('discord.js')
+const { Client, EmbedBuilder, ButtonBuilder, ActionRowBuilder, SelectMenuBuilder, Collection, PermissionFlagsBits } = require('discord.js')
 const client = new Client({intents: [3276799]})
 const fs = require("fs")
 const config = require('./config.json')
@@ -12,6 +12,31 @@ for(arx of archivos) {
     client.commands.set(comando.name, comando)
     console.log(`comando ${arx} iniciado correctamente`)
 }
+
+ const palabrasMalsonantes = ['palabra1', 'palabra2', 'palabra3'];
+
+
+ client.on('messageCreate', (message) => {
+  const contienePalabraMalsonante = palabrasMalsonantes.some((palabra) =>
+    message.content.toLowerCase().includes(palabra.toLowerCase())
+  );
+  
+  if (contienePalabraMalsonante) {
+    message.reply({content: "Palabra malsonante"})
+      .then(() => {
+        const member = message.member;
+        if (!member.permissions.has(PermissionFlagsBits.ManageMessages)) {
+          member.timeout(60000)
+            .then(() => message.channel.send(`${member.user.tag} ha sido silenciado por un minuto.`))
+            .catch(console.error);
+        } else {
+          console.log(`El miembro ${member.user.tag} no tiene los permisos necesarios para aplicar el timeout.`);
+        }
+      })
+      .catch(console.error);
+  }
+});
+
 client.on("messageCreate", async message =>{
     const prefix = "!"
     const args = message.content.slice(prefix.length).trim().split(/ +/);
@@ -38,6 +63,9 @@ client.on("interactionCreate", async interaction => {
      })
     } 
  })
+
+
+
 
 client.login(config.token)
 console.log("El bot está listo")
