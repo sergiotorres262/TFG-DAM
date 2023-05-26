e163e8054a39b3ae2fa2cc33c15c3a8e382a6a88
sergiotorres262 <sergio2002ojedatorres@gmail.com>
2023-05-26 20:42:03 +0200

    correccion comando help

--- comandos/help.js
+++ comandos/help.js
@@ -12,7 +12,7 @@ module.exports = {
         .addFields(
             { name: '!ban', value: 'Para banear a un usuario del servidor, su sintaxis es <!ban> <ID del usuario que quieres banear> EJ: !ban 1100775859717165167' },
             { name: '!kick', value: 'Para kickear a un usuario del servidor, su sintaxis es <!ban> <TAG del usuario que quieres kickear> <razon> EJ: !kick @Usuario spam' },
-            { name: '!unban', value: 'Para desbanear a un usuario del servidor, su sintaxis es <!unban> <TAG del usuario que quieres desbanear> EJ: !unban Usuario#7490' },
+            { name: '!unban', value: 'Para desbanear a un usuario del servidor, su sintaxis es <!unban> <TAG del usuario que quieres desbanear> EJ: !unban 754830011227439204' },
             { name: '!insert', value: 'Para insertar una palabra en la tabla de palabaras malsonantes, su sintaxis es <!insert><palabra que quieres insertar en la tabla>' },
             { name: '!delete', value: 'Para borrar una palabra de la tabla de palabaras malsonantes, su sintaxis es <!delete><palabra que quieres borrar de la tabla>' }
         )
